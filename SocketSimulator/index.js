ff1ec3c08568882b2ad69555fdbb8baf98c2ed4b
David An <dong82@gmail.com>
2022-04-09 12:43:16 -0400

    Add comments in socket simulator

--- SocketSimulator/index.js
+++ SocketSimulator/index.js
@@ -1,39 +1,51 @@
 require('dotenv').config({path: __dirname + '/.env'})
 const ws = require('aws-lambda-ws-server');
+const { handler: onConnect } = require('../onconnect/app');
+// const { handler: onDisonnect } = require('../ondisconnect/app');
+// const { handler: sendMessage } = require('../sendmessage/app');
 
 const clients = {};
 const handler = ws.handler({
     connect: async ({ id, event }) => {
+        console.log(`\nSimulating Gateway Routing:`);
+        console.log(`$connect> Client: ${id}`);
         clients[id] = true;
-        console.log('connected', id);
         return { statusCode: 200 };
     },
     disconnect: async ({ id }) => {
+        console.log(`\nSimulating Gateway Routing:`);
+        console.log(`$disconnect> Client: ${id}`);
         delete clients[id];
-        console.log('disconnect', id);
         return { statusCode: 200 };
     },
     default: async ({ message, id }) => {
-        console.log('default', message);
+        console.log(`\nSimulating Gateway Routing:`);
+        console.log(`$default> Client: ${id}`);
         return { statusCode: 200 };
     },
     echo: async ({ message, id, context }) => {
+        const methodName = 'echo';
+        console.log(`\nSimulating Gateway Routing:`);
+        console.log(`${methodName}> Client: ${id}`);
         const { postToConnection } = context;
         await postToConnection(message, id);
-        console.log('echo', message);
         return { statusCode: 200 };
     },
     sendmessage: async (req) => {
         // console.log('req: ', req);
         const { message, id, context } = req;
-        const clientIDs = Object.keys(clients);
-        console.log('sendmessage to all clients: ', clientIDs);
 
+        const methodName = 'sendmessage';
+        console.log(`\nSimulating Gateway Routing:`);
+        console.log(`${methodName}> Client: ${id}`);
+
+        const clientIDs = Object.keys(clients);        
         const msgPack = {
             senderId: id,
             message: message.text
         };
-
+        
+        console.log(`${methodName}> message to clients: ${clientIDs}`);
         await Promise.all(clientIDs.map(recipientId =>
             context.postToConnection(msgPack, recipientId)
         ));
