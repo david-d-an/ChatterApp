91898240317bdbc546616d817735aed02f469fb0
David An <dong82@gmail.com>
2022-04-09 18:26:07 -0400

    Update socket simulator

--- SocketSimulator/index.js
+++ SocketSimulator/index.js
@@ -1,54 +1,65 @@
 require('dotenv').config({path: __dirname + '/.env'})
 const ws = require('aws-lambda-ws-server');
-const { handler: onConnect } = require('../onconnect/app');
-// const { handler: onDisonnect } = require('../ondisconnect/app');
-// const { handler: sendMessage } = require('../sendmessage/app');
+const { handler: onConnect } = require('../onconnect');
+const { handler: onDisonnect } = require('../ondisconnect');
+const { handler: sendMessage } = require('../sendmessage');
 
-const clients = {};
+// const clients = {};
 const handler = ws.handler({
     connect: async ({ id, event }) => {
         console.log(`\nSimulating Gateway Routing:`);
         console.log(`$connect> Client: ${id}`);
-        clients[id] = true;
+
+        // clients[id] = true;
+        onConnect(event);
+
         return { statusCode: 200 };
     },
-    disconnect: async ({ id }) => {
+    disconnect: async ({ id, event }) => {
         console.log(`\nSimulating Gateway Routing:`);
         console.log(`$disconnect> Client: ${id}`);
-        delete clients[id];
+
+        // delete clients[id];
+        onDisonnect(event);
+
         return { statusCode: 200 };
     },
-    default: async ({ message, id }) => {
+    default: async ({ message, id, event, context }) => {
         console.log(`\nSimulating Gateway Routing:`);
         console.log(`$default> Client: ${id}`);
+
         return { statusCode: 200 };
     },
     echo: async ({ message, id, context }) => {
         const methodName = 'echo';
         console.log(`\nSimulating Gateway Routing:`);
         console.log(`${methodName}> Client: ${id}`);
+
         const { postToConnection } = context;
         await postToConnection(message, id);
+
         return { statusCode: 200 };
     },
     sendmessage: async (req) => {
         // console.log('req: ', req);
-        const { message, id, context } = req;
+        const { message, id, event, context } = req;
 
         const methodName = 'sendmessage';
         console.log(`\nSimulating Gateway Routing:`);
         console.log(`${methodName}> Client: ${id}`);
 
-        const clientIDs = Object.keys(clients);        
-        const msgPack = {
-            senderId: id,
-            message: message.text
-        };
-        
-        console.log(`${methodName}> message to clients: ${clientIDs}`);
-        await Promise.all(clientIDs.map(recipientId =>
-            context.postToConnection(msgPack, recipientId)
-        ));
+        // const clientIDs = Object.keys(clients);        
+        // const msgPack = {
+        //     senderId: id,
+        //     message: message.text
+        // };
+
+        // console.log(`${methodName}> message to clients: ${clientIDs}`);
+        // await Promise.all(clientIDs.map(recipientId =>
+        //     context.postToConnection(msgPack, recipientId)
+        // ));
+
+        sendMessage(event);
 
         return { statusCode: 200 };
     },
