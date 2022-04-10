49a01bc7e7d694a295e567d1fbef647d1cda9bf7
David An <dong82@gmail.com>
2022-04-09 23:37:46 -0400

    Relocate AWS_REGION_NAME

--- sendmessage/AwsFactory.js
+++ sendmessage/AwsFactory.js
@@ -1,12 +1,12 @@
 const AWS = require('aws-sdk');
 const { 
-  DYNAMO_END_POINT, 
   AWS_REGION_NAME, 
+  DYNAMO_END_POINT, 
 } = process.env;
 
 exports.GetSDK = () => {
-// console.log('# AWS_REGION_NAME: ', AWS_REGION_NAME);
-// console.log('# DYNAMO_END_POINT: ', DYNAMO_END_POINT);
+  // console.log('# AWS_REGION_NAME: ', AWS_REGION_NAME);
+  // console.log('# DYNAMO_END_POINT: ', DYNAMO_END_POINT);
   if (AWS_REGION_NAME === 'local') {
     console.log('Activating local DynamoDB');
     AWS.config.update({
